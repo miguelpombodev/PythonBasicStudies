5975ef650eda9d15115d917d6994f4505b023681
Miguel Pombo <mp96dev@gmail.com>
2022-07-29 21:24:35 -0300

    if/else/elif section

---
+++ secao_5/estruturas_logicas_and_or_not_is.py
@@ -0,0 +1,24 @@
+ativo = True
+logado = False
+
+# if ativo and logado:
+#     print("Bem-vindo usuário!")
+# else:
+#     print("Você precisa ativar sua conta. Por favor, cheque seu email")
+
+
+# if ativo or logado:
+#     print("Bem-vindo usuário!")
+# else:
+#     print("Você precisa ativar sua conta. Por favor, cheque seu email")
+
+# if not ativo:
+#     print("Você precisa ativar sua conta. Por favor, cheque seu email")
+
+# if ativo is True:
+#     print("Você precisa ativar sua conta. Por favor, cheque seu email")
+
+if ativo:  # Outro modo de ver se o valor é True
+    print("Bem-vindo usuário")
+else:
+    print("Você precisa ativar sua conta. Por favor, cheque seu email")
---
+++ secao_5/if_else_elif.py
@@ -0,0 +1,8 @@
+idade = 6
+
+if idade < 18:
+    print('Menor de idade')
+elif idade == 18:
+    print('Tem 18 anos')
+else:
+    print("Maior de idade")
